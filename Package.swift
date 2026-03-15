a11340663e5345deb521e440050616c06c89773d
Mathias Remshardt <remshams@gmail.com>
2026-03-15 19:17:45 +0100

    fix: resolve Testing framework from active Xcode

--- Package.swift
+++ Package.swift
@@ -2,11 +2,38 @@ import Foundation
 // swift-tools-version: 6.0
 import PackageDescription
 
-let developerDir = ProcessInfo.processInfo.environment["DEVELOPER_DIR"]
+func shellOutput(_ executable: String, arguments: [String]) -> String? {
+    let process = Process()
+    let outputPipe = Pipe()
+
+    process.executableURL = URL(fileURLWithPath: executable)
+    process.arguments = arguments
+    process.standardOutput = outputPipe
+    process.standardError = Pipe()
+
+    do {
+        try process.run()
+        process.waitUntilExit()
+    } catch {
+        return nil
+    }
+
+    guard process.terminationStatus == 0 else {
+        return nil
+    }
+
+    let outputData = outputPipe.fileHandleForReading.readDataToEndOfFile()
+    return String(data: outputData, encoding: .utf8)?
+        .trimmingCharacters(in: .whitespacesAndNewlines)
+}
+
+let developerDir =
+    ProcessInfo.processInfo.environment["DEVELOPER_DIR"]
+    ?? shellOutput("/usr/bin/xcode-select", arguments: ["-p"])
+
 let testingFrameworkPathCandidates = [
     developerDir.map { "\($0)/Platforms/MacOSX.platform/Developer/Library/Frameworks" },
     developerDir.map { "\($0)/Library/Developer/Frameworks" },
-    "/Library/Developer/CommandLineTools/Library/Developer/Frameworks",
 ].compactMap { $0 }
 let testingFrameworkPath = testingFrameworkPathCandidates.first {
     FileManager.default.fileExists(atPath: "\($0)/Testing.framework")
