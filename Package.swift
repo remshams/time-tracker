d06e235a2d0699697f92e89341ff3d323b7b4ee1
Mathias Remshardt <remshams@gmail.com>
2026-03-15 18:33:16 +0100

    style: add swift-format configuration

--- Package.swift
+++ Package.swift
@@ -1,12 +1,12 @@
+import Foundation
 // swift-tools-version: 6.0
 import PackageDescription
-import Foundation
 
 let developerDir = ProcessInfo.processInfo.environment["DEVELOPER_DIR"]
 let testingFrameworkPathCandidates = [
     developerDir.map { "\($0)/Platforms/MacOSX.platform/Developer/Library/Frameworks" },
     developerDir.map { "\($0)/Library/Developer/Frameworks" },
-    "/Library/Developer/CommandLineTools/Library/Developer/Frameworks"
+    "/Library/Developer/CommandLineTools/Library/Developer/Frameworks",
 ].compactMap { $0 }
 let testingFrameworkPath = testingFrameworkPathCandidates.first {
     FileManager.default.fileExists(atPath: "\($0)/Testing.framework")
@@ -32,15 +32,16 @@ let package = Package(
             },
             linkerSettings: testingFrameworkPath.map {
                 [
-                    .unsafeFlags([
-                        "-Xlinker", "-F",
-                        "-Xlinker", $0,
-                        "-Xlinker", "-rpath",
-                        "-Xlinker", $0
-                    ], .when(platforms: [.macOS])),
-                    .linkedFramework("Testing", .when(platforms: [.macOS]))
+                    .unsafeFlags(
+                        [
+                            "-Xlinker", "-F",
+                            "-Xlinker", $0,
+                            "-Xlinker", "-rpath",
+                            "-Xlinker", $0,
+                        ], .when(platforms: [.macOS])),
+                    .linkedFramework("Testing", .when(platforms: [.macOS])),
                 ]
             }
-        )
+        ),
     ]
 )
